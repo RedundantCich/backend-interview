a17986d017d36734d192ce68397439b7e3f51b6b
tomasz_bednarz_pl <35335768+RedundantCich@users.noreply.github.com>
2026-07-05 21:06:34 +0200

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    java
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.14.4")
+
+    testImplementation("io.rest-assured:rest-assured:5.5.7")
+
+    testImplementation("org.assertj:assertj-core:3.27.7")
+
+    testImplementation("org.wiremock:wiremock:3.13.2")
+
+    testImplementation("org.awaitility:awaitility:4.3.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
